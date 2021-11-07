fbad509bb055a842b675b200a60681bd9f38545e
ThesYu <thesyu@163.com>
2021-11-08 01:05:25 +0800

    v0.0.2

--- preload.js
+++ preload.js
@@ -8,14 +8,29 @@ window.exports = {
     args: {
       enter: (action) => {
         window.utools.hideMainWindow();
-        let ChangeDouble = String.raw `$out = reg query HKCU\SOFTWARE\Microsoft\InputMethod\Settings\CHS /v "Enable Double Pinyin"; $outStr = $out -as [string]; if ($outStr -match '(Pinyin.+0x)(\d)') {$mode = $Matches[2] } else { $mode = 1 }; if ($mode -as [int] -eq 1) { $Double = 0 } else { $Double = 1 }; reg.exe add HKCU\SOFTWARE\Microsoft\InputMethod\Settings\CHS /v "Enable Double Pinyin" /t REG_DWORD /d $Double /f;`;
-        exec(`echo ${ChangeDouble} | powershell.exe`, (error, stdout, stderr) => {
+        let ChangeDouble = 'reg add HKCU\\SOFTWARE\\Microsoft\\InputMethod\\Settings\\CHS /v "Enable Double Pinyin" /t REG_DWORD /d 0x1 /f';
+        exec(`${ChangeDouble}`, (error, stdout, stderr) => {
           if (error) {
-            window.utools.showNotification(`报错信息: ${error}`);
+            window.utools.showNotification(`${error}`);
           }
           window.utools.outPlugin();
         });
       }
     }
-  }
+  },
+  "ChangeAll": {
+    mode: "none",
+    args: {
+      enter: (action) => {
+        window.utools.hideMainWindow();
+        let ChangeAll = 'reg add HKCU\\SOFTWARE\\Microsoft\\InputMethod\\Settings\\CHS /v "Enable Double Pinyin" /t REG_DWORD /d 0x0 /f';
+        exec(`${ChangeAll}`, (error, stdout, stderr) => {
+          if (error) {
+            window.utools.showNotification(`${error}`);
+          }
+          window.utools.outPlugin();
+        });
+      }
+    }
+  },
 };
